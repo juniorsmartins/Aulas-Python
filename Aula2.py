97f911a5787b7f04df3d005bfe9d5c957826ba3e
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-13 13:14:45 -0400

    Aula Python
    Aula Python

--- Aula2.py
+++ Aula2.py
@@ -1,6 +1,10 @@
 
-print('Ola, Mundo!')
+print('\nOla, Mundo!')
 print(7 + 3)
 print('Ola', 5)
 
-
+nome = 'Junior'
+idade = 42
+peso = 128
+print('\n')
+print('Nome: ', nome, ' Idade: ', idade, ' Peso: ', peso)
