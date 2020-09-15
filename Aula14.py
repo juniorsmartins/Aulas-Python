89525fbc7fac09e59c5a826eda33a37c968cf8e3
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-15 17:28:07 -0400

    Aula Python
    Aula Python

---
+++ Aula14.py
@@ -0,0 +1,14 @@
+print('')
+print('-'*20)
+print('Curso em Vídeo')
+print('Professor: Gustavo Guanabara')
+print('-'*20)
+print('')
+
+mensagem = 'Olá, Mundo!'
+print(mensagem)
+print('')
+
+nome=str(input('Qual teu nome? '))
+print('Seja bem-vindo, {}'.format(nome))
+
