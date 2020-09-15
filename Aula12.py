37df16f9d5a4dd515756748362f66f1d3adcb855
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-15 17:16:36 -0400

    Aula Python
    Aula Python

--- Aula12.py
+++ Aula12.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 print('')
 print('Estudo de Python')
 print('Livro: Python para desenvolvedores - Luiz Eduardo Borges')
@@ -9,13 +8,15 @@ nome = input('Qual teu nome? ')
 nota1 = float(input('Qual tua primeira nota? '))
 nota2 = float(input('Qual tua segunda nota? '))
 nota3 = float(input('Qual tua terceira nota? '))
-media = (nota1 + nota2 + nota3)/3
+nota_media = (nota1 + nota2 + nota3)/3
 
-if 0 <= media < 7:
-  print 'Reprovado!'
-elif 7 <= media < 9: 
-  print 'Aprovado!'
-elif 9 <= media <= 10:
-  print 'Aprovado com louvor!'
+if nota_media < 0:
+  print('Nota c/ Erro!')
+elif 0 <= nota_media < 7:
+  print('Reprovado!')
+elif 7 <= nota_media < 9: 
+  print('Aprovado!')
+elif 9 <= nota_media <= 10:
+  print('Aprovado com louvor!')
 else: 
-  print 'Nota inexistente!'
+  print('Nota inexistente!')
---
+++ Aula13.py
@@ -0,0 +1,19 @@
+print('')
+print('-'*20)
+print('Curso em Vídeo')
+print('Professor: Gustavo Guanabara')
+print('Operações Aritméticas')
+print('-'*20)
+print('')
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, a multiplicação é {} e a divisão é {:.2f}'.format(s, m, d))
+print('A divisão inteira é {} e a potência é {}'.format(di, e))
+print('-'*40)
+print('')
