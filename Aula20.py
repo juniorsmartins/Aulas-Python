7b404ed761a871812d3de156fc90b2be0057a6e2
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-22 18:33:55 -0400

    Aula Python
    Aula Python

---
+++ Aula20.py
@@ -0,0 +1,15 @@
+print("")
+print("Linguagem de Programação - Python")
+print("Teste de For")
+print("")
+
+for contador in range (1, 11):
+  print("Tabuada - {} x 10 = {}".format(contador, (contador * 10)))
+
+lista = [42, 'Ovo', 52, 'Maça', 'Pera']
+for item in lista:
+  print("O item atual é: ", item)
+
+print("")
+print("Fim do programa!")
+print("")
