d43ff3ebda88370122d7329fa1f1cf4be6b351b7
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-22 18:05:21 -0400

    Aula Python
    Aula Python

--- Aula19.py
+++ Aula19.py
@@ -7,14 +7,19 @@ print("Aprendendo Python")
 print("")
 print("Programa - Tente acertar o número sorteado")
 print("")
-valor_Sorteado = random.randint(0, 10)
+valor_Sorteado = random.randint(1, 10)
 valor_Escolhido = 0
 
 while valor_Escolhido != valor_Sorteado :
+  print("")
   valor_Escolhido = int(input("Digite um número inteiro de 0 até 10: "))
+  print("")
   if valor_Escolhido == valor_Sorteado :
     print("Você acertou!")
+    print("")
   elif (valor_Escolhido != valor_Sorteado):
     print("Você errou!")
     print("Tente novamente!")
-    time.sleep(4)
+    print("")
+    input("Tecle <enter> para continuar!")
+    print("")
