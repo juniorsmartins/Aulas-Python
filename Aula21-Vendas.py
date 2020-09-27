873766f438890ead0f6c53d5cdc65751b522538a
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-26 21:51:30 -0400

    Aula Python
    Aula Python

---
+++ Aula21-Vendas.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+do 
+print("\n"*5)
+print("-"*40)
+print("-"*15 + "   Menu   " + "-"*15)
+print("-"*40)
+print("-"*13 + "  Vender - 1  " + "-"*13)
+print("-"*11 + "  Consultar - 2  " + "-"*12)
+print("-"*11 + "  Cadastrar - 3  " + "-"*12)
+print("-"*12 + "  Alterar - 4  " + "-"*13)
+print("-"*12 + "  Excluir - 5  " + "-"*13)
+print("-"*12 + "  Relatar - 6  " + "-"*13)
+print("-"*14 + "  Sair - 0  " + "-"*14)
+print("-"*40)
+opcao_Principal = int(input("-"*10 + "  Qual opção? "))
+
+
+
+
